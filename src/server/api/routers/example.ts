61b18778a10f4acbe243b0c2e691487927b3e0fd
niksbjelo <71076431+niksbjelo@users.noreply.github.com>
2023-05-23 09:22:37 +0000

    adding dx

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { env } from "~/env.mjs";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
@@ -6,6 +7,7 @@ export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
+      console.log(env.BACKEND_BROWSERLESS);
       return {
         greeting: `Hello ${input.text}`,
       };
